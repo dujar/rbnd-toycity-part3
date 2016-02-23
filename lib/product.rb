1487f09e49bb4c98e3f087735034b1e9659de7cf
dujar <dujar.coding@gmail.com>
2016-02-23 15:26:41 +0100

    Add ability to create and count products

--- lib/product.rb
+++ lib/product.rb
@@ -3,15 +3,19 @@ class Product
 
     attr_reader :title, :price, :stock
 
+    @@products= []
+    @@id = 1
     def initialize options = {}
+        @id = @@id
+        @@id += 1
         @title = options[:title]
         @price = options[:price]
         @stock = options[:stock]
-        @@product = {}
+        @@products << self
     end
 
     def self.all
-        @product = {@title, @price, @stock}
+        @@products
     end
 
 end
