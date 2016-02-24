c3638c75728349bc6728205d7aa085366b959c0b
dujar <dujar.coding@gmail.com>
2016-02-24 11:38:39 +0100

    in_stock? n find_by_title

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,7 @@
 
 class Product
 
-    attr_reader :title, :price, :stock
+    attr_accessor :title, :price, :stock
 
     @@products= []
     @@id = 1
@@ -11,11 +11,66 @@ class Product
         @title = options[:title]
         @price = options[:price]
         @stock = options[:stock]
-        @@products << self
+        add_to_products
+
     end
 
     def self.all
         @@products
     end
 
+    def add_to_products
+        if @@id > 1
+            if initializers.include? @title
+                raise DuplicateProductError, "'#{@title}' already exists!"
+            else
+        @@products << self
+            end
+        end
+    end
+
+    def initializers 
+        titles = []
+        prices = []
+        stocks = []
+            @@products.each do |item|
+                titles << item.title
+                prices << item.price
+                stocks << item.stock
+            end
+        titles
+    end
+
+
+    def self.find_by_title selected_title
+        @@products.each do |item|
+            if item.title == selected_title
+                found = item
+                return found
+            end
+
+        end
+    end
+
+    def self.in_stock
+        stocks = []
+            @@products.each do |item|
+                stocks << item.stock
+            end
+        return stocks
+    end
+
+
+    def in_stock?
+        if stock > 0
+            true
+        else 
+            false
+        end
+    end
+
+
+
+
 end
+
